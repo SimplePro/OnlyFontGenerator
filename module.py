22a1e20235d04107b532c8b3e101165ccb531688
SimplePro <66504341+SimplePro@users.noreply.github.com>
2022-09-09 19:40:45 +0900

    added module.py

---
+++ module.py
@@ -0,0 +1,141 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+
+
+
+def extract_content(generator, content_letters, could_adain=False):
+    content_list = [content_letters]
+
+    for i in range(len(generator.content_extractor)):
+        content_list.append(generator.content_extractor[i](content_list[-1]))
+
+    return content_list
+
+
+def extract_style(generator, style_labels):
+    style = generator.style_extractor(style_labels)
+
+    return style
+
+
+@torch.no_grad()
+def show_pred(device, generator, dataloader, n=10):
+
+    content_letters, style_letters, style_labels = dataloader.get(batch_size=n)
+
+    content_letters = content_letters.to(device).type(torch.float32)
+    style_letters = style_letters.to(device).type(torch.float32)
+    style_labels = style_labels.to(device).type(torch.float32)
+
+    content_images = (content_letters.cpu().detach().numpy() * 255).astype(np.uint)
+    style_images = (style_letters.cpu().detach().numpy() * 255).astype(np.uint)
+
+    pred = generator(content_letters, style_labels)
+    pred_images = (pred.cpu().detach().numpy() * 255).astype(np.uint)
+
+    plt.figure(figsize=(n, 3*n))
+
+    for i in range(n):
+
+        content_img = content_images[i].transpose(1, 2, 0).reshape(128, 128)
+        style_img = style_images[i].transpose(1, 2, 0).reshape(128, 128)
+        pred_img = pred_images[i].transpose(1, 2, 0).reshape(128, 128)
+
+        plt.subplot(n, 3, 3*i+1)
+        plt.imshow(content_img, cmap="gray")
+        plt.axis("off")
+
+        plt.subplot(n, 3, 3*i+2)
+        plt.imshow(style_img, cmap="gray")
+        plt.axis("off")
+
+        plt.subplot(n, 3, 3*i+3)
+        plt.imshow(pred_img, cmap="gray")
+        plt.axis("off")
+
+
+@torch.no_grad()
+def pred_by_style_content(generator, content_list, style_features):
+    latent_vector = torch.cat((content_list[-1], style_features), dim=1)
+    out = latent_vector
+
+    for i in range(len(generator.generator)):
+        out = generator.generator[i](out)
+
+        if i < 3:
+            out += F.interpolate(content_list[-i-1], scale_factor=2.0)
+
+    return out
+
+
+# @torch.no_grad()
+# def pred_by_style_content_adain(generator, content_features, style_features):
+#         constant_input = generator.constant_input(content_letters)
+
+#         gray_outs = []
+
+#         out = generator.main[0](constant_input, content_features[:, :2, :])
+#         gray_outs.append(generator.to_gray[0](out))
+
+#         out = generator.main[1](out, content_features[:, 2:4, :])
+#         gray_outs.append(generator.to_gray[1](out))
+
+#         out = generator.main[2](out, content_features[:, 4:6, :])
+#         gray_outs.append(generator.to_gray[2](out))
+
+#         out = generator.main[3](out, style_features[:, 0:2, :])
+#         gray_outs.append(generator.to_gray[3](out))
+
+#         out = generator.main[4](out, style_features[:, 2:4, :])
+#         gray_outs.append(generator.to_gray[4](out))
+
+#         out = generator.main[5](out, style_features[:, 4:6, :])
+#         gray_outs.append(generator.to_gray[5](out))
+
+#         out = gray_outs[0]
+
+#         for i in range(1, len(gray_outs)):
+#             out = generator.upsampling(out) + gray_outs[i]
+
+#         out = torch.sigmoid(out)
+
+#         return out
+
+
+# @torch.no_grad()
+# def interpolate_adain(generator, save_dir, content_features, start_style_features, end_style_features, step_n=100):
+#     step_gap = (end_style_features - start_style_features) / step_n
+
+#     style = start_style_features
+
+#     for step in range(step_n):
+#         pred = pred_by_style_content_adain(generator, content_features, style) # (1, 1, 128, 128)
+#         pred_img = transforms.ToPILImage()(pred.cpu().detach().squeeze(0))
+        
+#         pred_img.save(f"{save_dir}/step{step}.png")
+
+#         style += step_gap
+
+
+@torch.no_grad()
+def interpolate(generator, save_dir, content_list, start_style_features, end_style_features, step_n=100):
+    step_gap = (end_style_features - start_style_features) / step_n
+
+    style = start_style_features
+
+    for step in range(step_n):
+        pred = pred_by_style_content(generator, content_list, style) # (1, 1, 128, 128)
+        pred_img = transforms.ToPILImage()(pred.cpu().detach().squeeze(0))
+        
+        pred_img.save(f"{save_dir}/step{step}.png")
+
+        style += step_gap
+
+
+def make_gif(paths, save_path, fps=500):
+    img, *imgs = [Image.open(path) for path in paths]
+    img.save(fp=save_path, format="GIF", append_images=imgs, save_all=True, duration=fps, loop=1)
